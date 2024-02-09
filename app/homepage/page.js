82bee4d81e7c1d5e3af96ff1418df6c6bae58b1c
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-02-09 11:33:51 +0000

    event cards completed

--- app/homepage/page.js
+++ app/homepage/page.js
@@ -3,44 +3,47 @@ import { useEffect, useState } from "react";
 import { ApiClient } from "@/apiClient";
 import Dashboard from "@/app/dashboard/dashboard";
 import AuthoriseUser from "@/app/components/AuthoriseUser";
+import Card from "../components/Card";
+import CardContainer from "../components/CardContainer";
 
 
 export default function Home() {
-  const [token, setToken] = useState(null);
-  const client = new ApiClient(
-    () => token,
-    () => logout()
-  );
+  // const [token, setToken] = useState(null);
+  // const client = new ApiClient(
+  //   () => token,
+  //   () => logout()
+  // );
 
-  useEffect(() => {
-    console.log("Page Log out")
-  }, [token]);
+  // useEffect(() => {
+  //   console.log("Page Log out")
+  // }, [token]);
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setToken(token);
-    } // if !token, redirect to landing page
-  }, []);
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   if (token) {
+  //     setToken(token);
+  //   } // if !token, redirect to landing page
+  // }, []);
 
-  const login = (token) => {
-    localStorage.setItem("token", token);
-    setToken(token);
-  };
+  // const login = (token) => {
+  //   localStorage.setItem("token", token);
+  //   setToken(token);
+  // };
 
-  const logout = () => {
-    localStorage.removeItem("token");
-    setToken(null);
-  };
+  // const logout = () => {
+  //   localStorage.removeItem("token");
+  //   setToken(null);
+  // };
 
   return (
     <>
-      {token ? (
+      {/* {token ? (
         <Dashboard setToken={setToken} client={client}/>
       ) : (
         <AuthoriseUser loggedIn={(token) => login(token)} client={client} />
 
-      )}
+      )} */}
+      <CardContainer />
     </>  
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,56 +1,56 @@
-"use client";
-// import { useEffect, useState } from "react";
-import Link from 'next/link';
-// import { ApiClient } from "@/apiClient";
-// import Dashboard from "@/app/dashboard/dashboard";
-// import AuthoriseUser from "@/app/components/AuthoriseUser";
-// import GetStartedButton from "@app/components/GetStartedButton";
+// "use client";
+// // import { useEffect, useState } from "react";
+// import Link from 'next/link';
+// // import { ApiClient } from "@/apiClient";
+// // import Dashboard from "@/app/dashboard/dashboard";
+// // import AuthoriseUser from "@/app/components/AuthoriseUser";
+// // import GetStartedButton from "@app/components/GetStartedButton";
 
 
 
-export default function Home() {
-  // const [token, setToken] = useState(null);
-  // const client = new ApiClient(
-  //   () => token,
-  //   () => logout()
-  // );
+// export default function Home() {
+//   // const [token, setToken] = useState(null);
+//   // const client = new ApiClient(
+//   //   () => token,
+//   //   () => logout()
+//   // );
 
-  // useEffect(() => {
-  //   console.log("Page Log out")
-  // }, [token]);
+//   // useEffect(() => {
+//   //   console.log("Page Log out")
+//   // }, [token]);
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   if (token) {
-  //     setToken(token);
-  //   } // if !token, redirect to landing page
-  // }, []);
+//   // useEffect(() => {
+//   //   const token = localStorage.getItem("token");
+//   //   if (token) {
+//   //     setToken(token);
+//   //   } // if !token, redirect to landing page
+//   // }, []);
 
-  // const login = (token) => {
-  //   localStorage.setItem("token", token);
-  //   setToken(token);
-  // };
+//   // const login = (token) => {
+//   //   localStorage.setItem("token", token);
+//   //   setToken(token);
+//   // };
 
-  // const logout = () => {
-  //   localStorage.removeItem("token");
-  //   setToken(null);
-  // };
+//   // const logout = () => {
+//   //   localStorage.removeItem("token");
+//   //   setToken(null);
+//   // };
   
 
-  return (
-    // <>
-    //   {token ? (
-    //     <Dashboard setToken={setToken} client={client}/>
-    //   ) : (
-    //     <AuthoriseUser loggedIn={(token) => login(token)} client={client} />
+//   return (
+//     // <>
+//     //   {token ? (
+//     //     <Dashboard setToken={setToken} client={client}/>
+//     //   ) : (
+//     //     <AuthoriseUser loggedIn={(token) => login(token)} client={client} />
 
-    //   )}
-    // </> 
-   <div className="bg-black h-screen">
-    <Link href='/homepage'>
-            <button
-               className='text-white  font-bold rounded-lg uppercase text-sm px-2 py-2.5 text-center me-2 mb-2'>Home</button>
-          </Link>
-    </div>
-  );
-}
+//     //   )}
+//     // </> 
+//    <div className="bg-black h-screen">
+//     <Link href='/homepage'>
+//             <button
+//                className='text-white  font-bold rounded-lg uppercase text-sm px-2 py-2.5 text-center me-2 mb-2'>Home</button>
+//           </Link>
+//     </div>
+//   );
+// }
