7e5c0818e371ae35d76d1d5f8de379726b7ad248
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-02-29 13:41:12 +0000

    event form toggle

--- app/components/EventCard.js
+++ app/components/EventCard.js
@@ -31,23 +31,28 @@ const EventCard = (props) => {
       {/* <h3 className="mt-5 mb-2 text-cyan-500 text-center">Your Events</h3> */}
       <div
         className="w-80 h-64 relative mb-5"
-        onClick={
+        // onClick={
+        //   () => {
+        //     handleClickOpen(props)
+        //   }
+        // }
+      >
+        <img className="object-cover w-full h-full rounded-xl" src={props.photo} alt="EventPhoto" />
+
+        <div className="bg-black/50 absolute top-0 text-white w-full h-full text-center flex flex-col justify-center border-4 border-black hover:border-4 hover:border-[#1AA297] hover:cursor-pointer rounded-xl" onClick={
           () => {
             handleClickOpen(props)
           }
-        }
-      >
-        <img className="object-cover w-screen rounded-xl" src={props.photo} alt="EventPhoto" />
-
-        <div className="bg-black/50 absolute top-0 text-white w-full h-full text-center flex flex-col justify-center border-4 border-black hover:border-4 hover:border-[#1AA297] hover:cursor-pointer rounded-xl">
+        }>
           <p className="text-base font-bold"> {props.EventName}</p>
           <p className="text-sm font-medium mt-2">
             {props.EventDate} - {props.EventTime}
           </p>
           <p className="text-xs mt-2">{props.EventVenue} - {props.EventCity}, {props.EventCountryCode}, {props.EventPostcode}</p>
           <p className="text-xs mt-2">Price: {props.EventCurrency}{props.EventPrice} - {props.EventCurrency}{props.EventPriceMax}</p>
+        </div>
 
-          <div className="col-span-1 md:col-span-2 flex gap-10 mx-20 justify-center space-x-4 mt-5">
+          <div className="absolute bottom-0 -left-7 col-span-1 md:col-span-2 flex gap-10 mx-20 justify-center space-x-4 mt-5">
             <button
               type="submit"
               className="bg-[#13C3B5] p-4 w-[55%] font-semibold text-white rounded-full hover:text-white hover:bg-[#534A4A] focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 transition-all duration-300 ease-in-out mt-10"
@@ -66,7 +71,6 @@ const EventCard = (props) => {
               Delete
             </button>
           </div>
-        </div>
         <EventUserCardExpanded
         open={open}
         handleClose={handleClose}
